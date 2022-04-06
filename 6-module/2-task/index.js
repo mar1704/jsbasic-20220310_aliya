67d2e401695bed3c7b98296b1419a86b59e132f9
Aliya Mukhetdinova <aliya_muhetdinova@mail.ru>
2022-04-06 14:41:53 +0300

    6_2_ProductCardClass

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,4 +1,47 @@
+import createElement from '../../assets/lib/create-element.js';
 export default class ProductCard {
+  
   constructor(product) {
+    this.img = product.image;
+    this.name = product.name;
+    this.price = product.price;
+    this.category = product.category;
+    this.id = product.id;
+    this.elem = this.CreateDiv();
+    this.btnevent = this.BtnClick()
   }
+
+  CreateDiv(){
+    const div = createElement(`
+      <div class="card">
+        <div class="card__top">
+          <img src="/assets/images/products/${this.img}" class="card__image" alt="product">
+          <span class="card__price">€${this.price.toFixed(2)}</span>
+        </div>
+        <div class="card__body">
+          <div class="card__title">${this.name}</div>
+          <button type="button" class="card__button">
+            <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+          </button>
+        </div>
+      </div>
+      `)
+      
+      return div;        
+  }
+  
+  BtnClick() {
+    const btn = this.elem.querySelector('.card__button');
+
+    btn.addEventListener('click', () => {
+    
+        let Clickevent = new CustomEvent("product-add", { // имя события должно быть именно "product-add"
+          detail: this.id, // Уникальный идентификатора товара из объекта товара
+          bubbles: true // это событие всплывает - это понадобится в дальнейшем
+        });
+        
+        btn.dispatchEvent(Clickevent);
+    });
+  }
+   
 }
