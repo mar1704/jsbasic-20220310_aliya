437c7454aef5a3511229184ee7b8a661a2094631
Aliya <bugaga789@yandex.ru>
2022-10-02 21:07:05 +0300

    8.3

--- 8-module/3-task/index.js
+++ 8-module/3-task/index.js
@@ -3,25 +3,54 @@ export default class Cart {
 
   constructor(cartIcon) {
     this.cartIcon = cartIcon;
-    this.cartItems =[];
+    
   }
 
   addProduct(product) {
-   
-    
-    
+    if (!product) {
+      return;
+    }
+
+    let cartItem = this.cartItems.find(
+      item => item.product.id === product.id
+    );
+
+    if (!cartItem) {
+      cartItem = {
+        product,
+        count: 1
+      };
+      this.cartItems.push(cartItem);
+    } else {
+      cartItem.count++;
+    }
+
+    this.onProductUpdate(cartItem);
   }
 
   updateProductCount(productId, amount) {
-    // ваш код
+    let cartItem = this.cartItems.find(item => item.product.id == productId);
+    cartItem.count += amount;
+
+    if (cartItem.count === 0) {
+      this.cartItems.splice(this.cartItems.indexOf(cartItem), 1);
+    }
+
+    this.onProductUpdate(cartItem);
+  }
+
+  onProductUpdate() {
+    // реализуем в следующей задаче
+
+    this.cartIcon.update(this);
   }
 
   isEmpty() {
-    // ваш код
+    return this.cartItems.length === 0;
   }
 
   getTotalCount() {
-    // ваш код
+    return this.cartItems.reduce((sum, item) => sum + item.count, 0);
   }
 
   getTotalPrice() {
@@ -32,6 +61,10 @@ export default class Cart {
     // реализуем в следующей задаче
 
     this.cartIcon.update(this);
+    return this.cartItems.reduce(
+      (sum, item) => sum + item.product.price * item.count,
+      0
+    );
   }
 }
 
